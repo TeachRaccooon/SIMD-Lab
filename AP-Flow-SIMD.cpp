316c250d4d3ffa6689dbbb3937b9008af29de61e
Melnichenko Max <thrcn@TeachRaccoon>
2022-12-07 15:30:11 -0500

    Update

--- AP-Flow-SIMD.cpp
+++ AP-Flow-SIMD.cpp
@@ -1,5 +1,24 @@
 #include "AP-Flow.h"
 
+
+void APFlow::CalcFlow()
+{
+  int i, j, v, f;
+
+  for (i = 0; i < N*N; i++) Flow[i] = Adj[i];
+
+  for (v = 0; v < N; v++) {
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++) {
+        f = (Flow[i*N+v] < Flow[v*N+j]) ? Flow[i*N+v] : Flow[v*N+j];
+        if (f > Flow[i*N+j]) Flow[i*N+j] = f;
+      }   
+    }
+  }
+}
+
+
+/*
 void APFlow::CalcFlow()
 {
   int i, j, v, f;
@@ -12,10 +31,11 @@ void APFlow::CalcFlow()
         {
             for (j = 0; j < N; ++j) 
             {
-                /* f is the flow from i to j through v */
+                // f is the flow from i to j through v
                 f = (Flow[i * N + v] < Flow[v * N + j]) ? Flow[i * N + v] : Flow[v * N + j];
                 if (f > Flow[i * N + j]) Flow[i * N + j] = f;
             }   
         }
     }
 }
+*/
